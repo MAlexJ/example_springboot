ba6a298c12aae2002c371445e9598ac1cf616038
malexj <alexmaximovj@gmail.com>
2024-01-26 12:55:15 +0200

    Add dmd-courses examples

---
+++ dmdev-base-springboot-course/src/main/java/com/malexj/dmdevbasespringbootcourse/DmdevBaseSpringbootCourseApplication.java
@@ -0,0 +1,12 @@
+package com.malexj.dmdevbasespringbootcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmdevBaseSpringbootCourseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DmdevBaseSpringbootCourseApplication.class, args);
+  }
+}
---
+++ dmdev-base-springboot-course/src/test/java/com/malexj/dmdevbasespringbootcourse/DmdevBaseSpringbootCourseApplicationTests.java
@@ -0,0 +1,11 @@
+package com.malexj.dmdevbasespringbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmdevBaseSpringbootCourseApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
