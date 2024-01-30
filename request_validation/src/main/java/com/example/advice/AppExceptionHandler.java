246e1438921b002628869fd3f5853f7750dfda31
malexj <alexmaximovj@gmail.com>
2024-01-30 14:55:12 +0200

    Add simple handler and property for error message

---
+++ request_validation/src/main/java/com/example/advice/AppExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.advice;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Component
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  protected ResponseEntity<Object> handleConflict(MethodArgumentNotValidException ex) {
+    return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+  }
+}
