587df6d472eb845538ebdaf4f147ee4f407ad6b0
malexj <alexmaximovj@gmail.com>
2024-02-21 19:33:33 +0200

    Add base rest service

---
+++ base-rest-app/src/main/java/com/malexj/BaseRestAppApplication.java
@@ -0,0 +1,12 @@
+package com.malexj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseRestAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BaseRestAppApplication.class, args);
+  }
+}
