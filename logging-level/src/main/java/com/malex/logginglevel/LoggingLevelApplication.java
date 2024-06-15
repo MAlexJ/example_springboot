8ecbf81383982f9be8aad3e8987722f002600992
malexj <alexmaximovj@gmail.com>
2024-06-15 17:10:24 +0300

    Add loging level samples

---
+++ logging-level/src/main/java/com/malex/logginglevel/LoggingLevelApplication.java
@@ -0,0 +1,12 @@
+package com.malex.logginglevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingLevelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoggingLevelApplication.class, args);
+  }
+}
---
+++ logging-level/src/test/java/com/malex/logginglevel/LoggingLevelApplicationTests.java
@@ -0,0 +1,11 @@
+package com.malex.logginglevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingLevelApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
