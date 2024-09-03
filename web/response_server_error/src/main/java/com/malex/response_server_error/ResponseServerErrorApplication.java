e3b027d3091ffa8326339cfe30a2d1b043826339
malexj <alexmaximovj@gmail.com>
2024-09-03 18:24:45 +0300

    Add samples

---
+++ web/response_server_error/src/main/java/com/malex/response_server_error/ResponseServerErrorApplication.java
@@ -0,0 +1,12 @@
+package com.malex.response_server_error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseServerErrorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ResponseServerErrorApplication.class, args);
+  }
+}
---
+++ web/response_server_error/src/test/java/com/malex/response_server_error/ResponseServerErrorApplicationTests.java
@@ -0,0 +1,11 @@
+package com.malex.response_server_error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponseServerErrorApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
