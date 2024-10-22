9a7bf555ecf15cbc90a8ce9acc6ff353d553774d
malexj <alexmaximovj@gmail.com>
2024-10-22 19:25:14 +0300

    Reb=name modules

--- events/simple-application-event/src/test/java/com/malex/simpleapplicationevent/SimpleApplicationEventApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.malex.simpleapplicationevent;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SimpleApplicationEventApplicationTests {
-
-  void contextLoads() {}
-}
